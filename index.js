d9d492508c2200f805b4e8e046a5c36d61395e6d
yakshit savaliya <yakshitsavaliya@gmail.com>
2026-04-13 11:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { app, BrowserWindow, desktopCapturer, ipcMain } = require('electron');
+// DesktopCapture - used for the screen recording / screen sharing.
+// app - process controller
+// BrowserWindow - Chrome tab into desktop window. 
+// ipcMain - Enable the between main process & renderer ( backend, frontend communication like API )
+
+// Electron = Rnederer(Frontend + UI) + Main Process(brain-Backend)
 const path = require('path');
 
-// Crucial step: Disable hardware acceleration to bypass DRM "black screens" (if decoding encrypted streams)
+// Main LOgic:  Disable hardware acceleration to bypass DRM(digital Rights Managemnt) "black screens" (if decoding encrypted streams)
 app.disableHardwareAcceleration();
 
 function createWindow() {
@@ -9,22 +15,24 @@ function createWindow() {
     width: 1100,
     height: 850,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      webviewTag: true
+      nodeIntegration: true, // enable the htm/css to use ther NodeJs
+      webviewTag: true // used to embed external sites
     }
   });
 
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('index.html'); // loaads the frontend. 
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow); // the createWindow is run when the App is Ready. 
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on('window-all-closed', () => { // here .on is like When THIS happens → DO THIS as in default in other Os here by closing all the window the app dosent Quit in MacOS. 
+  if (process.platform !== 'darwin') app.quit();  // darwin means MacOS When all windows close: If NOT Mac → quit app, If Mac → keep app running  default it can be left empty. 
 });
 
 // IPC Handler securely providing access to local window/desktop sources
 ipcMain.handle('get-sources', async () => {
     return await desktopCapturer.getSources({ types: ['window', 'screen'] });
 });
+//ipcMain → lives in main process, .handle → used for request-response type communication.
+// 'get-sources' is like API endpoint name. 
+// window for the individual app and Screen for like full screen etc. 
